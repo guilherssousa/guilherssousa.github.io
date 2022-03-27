f6d9a606ef5db7bbecfb98d2c25a75a1e89ffe57
guilherssousa <guilherssousa@gmail.com>
2022-03-27 19:41:43 -0300

    Add pwa-register types

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+import { registerSW } from "virtual:pwa-register";
+
+if ("serviceWorker" in navigator) {
+  registerSW();
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
