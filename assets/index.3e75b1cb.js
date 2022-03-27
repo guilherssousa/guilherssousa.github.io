bf3cd0357c16c7de7f93b45e778c1bed36e9c221
guilherssousa <guilherssousa@gmail.com>
2022-03-26 22:00:23 -0300

    PWA

--- assets/index.3e75b1cb.js
+++ assets/index.3e75b1cb.js
@@ -1,4 +1,4 @@
-import{r as d,W as y,s as n,j as x,g as E,I as C,H as $,Z as S,B as A,a as z,b as T,c as j,d as F,e as L,F as R,f as I,R as P,h as M,i as B,k as O}from"./vendor.8550df60.js";const N=function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const a of document.querySelectorAll('link[rel="modulepreload"]'))i(a);new MutationObserver(a=>{for(const l of a)if(l.type==="childList")for(const m of l.addedNodes)m.tagName==="LINK"&&m.rel==="modulepreload"&&i(m)}).observe(document,{childList:!0,subtree:!0});function s(a){const l={};return a.integrity&&(l.integrity=a.integrity),a.referrerpolicy&&(l.referrerPolicy=a.referrerpolicy),a.crossorigin==="use-credentials"?l.credentials="include":a.crossorigin==="anonymous"?l.credentials="omit":l.credentials="same-origin",l}function i(a){if(a.ep)return;a.ep=!0;const l=s(a);fetch(a.href,l)}};N();const U="@guilherssousa:";function D(o,t){const s=U+o,[i,a]=d.exports.useState(()=>{const l=localStorage.getItem(s);return l!=null?JSON.parse(l):typeof t=="function"?t():t});return d.exports.useEffect(()=>{localStorage.setItem(s,JSON.stringify(i))},[s,i]),[i,a]}const G=y`
+import{r as d,W as y,s as n,j as x,g as E,I as C,H as $,Z as S,B as A,a as z,b as T,c as j,d as F,e as L,F as R,f as I,R as P,h as M,i as B,k as N}from"./vendor.8550df60.js";const O=function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const a of document.querySelectorAll('link[rel="modulepreload"]'))i(a);new MutationObserver(a=>{for(const l of a)if(l.type==="childList")for(const m of l.addedNodes)m.tagName==="LINK"&&m.rel==="modulepreload"&&i(m)}).observe(document,{childList:!0,subtree:!0});function s(a){const l={};return a.integrity&&(l.integrity=a.integrity),a.referrerpolicy&&(l.referrerPolicy=a.referrerpolicy),a.crossorigin==="use-credentials"?l.credentials="include":a.crossorigin==="anonymous"?l.credentials="omit":l.credentials="same-origin",l}function i(a){if(a.ep)return;a.ep=!0;const l=s(a);fetch(a.href,l)}};O();const D="@guilherssousa:";function U(o,t){const s=D+o,[i,a]=d.exports.useState(()=>{const l=localStorage.getItem(s);return l!=null?JSON.parse(l):typeof t=="function"?t():t});return d.exports.useEffect(()=>{localStorage.setItem(s,JSON.stringify(i))},[s,i]),[i,a]}const H=y`
   * {
     padding: 0;
     margin: 0;
@@ -25,12 +25,12 @@ import{r as d,W as y,s as n,j as x,g as E,I as C,H as $,Z as S,B as A,a as z,b a
     background: #ffffff40;
     border-radius: 10px;
   }
-`,X={name:"dark",backgroundColor:"#1e293b",primary:"#f97316",secondary:"#fff",textColor:"#fff",backgroundGradient:"linear-gradient(83.2deg, rgba(33, 31, 88, 1) 10.8%, rgba(43, 54, 87, 1) 94.3%)",patternStrokeColor:"rgb(240 232 213 / 0.08)"},J={name:"light",backgroundColor:"#FEFEFE",primary:"#f97316",secondary:"#222",textColor:"#121212",patternStrokeColor:"rgb(15 23 42 / 0.04)"},q=n.div`
+`,J={name:"dark",backgroundColor:"#1e293b",primary:"#f97316",secondary:"#fff",textColor:"#fff",backgroundGradient:"linear-gradient(83.2deg, rgba(33, 31, 88, 1) 10.8%, rgba(43, 54, 87, 1) 94.3%)",patternStrokeColor:"rgb(240 232 213 / 0.08)"},X={name:"light",backgroundColor:"#FEFEFE",primary:"#f97316",secondary:"#222",textColor:"#121212",patternStrokeColor:"rgb(15 23 42 / 0.04)"},G=n.div`
   background: ${o=>o.theme.backgroundColor};
   min-height: 100vh;
   color: ${o=>o.theme.textColor};
   position: relative;
-`,H=n.div`
+`,q=n.div`
   background: ${o=>{const t=`url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width='32' height='32' fill='none' stroke='${o.theme.patternStrokeColor}'%3e%3cpath d='M0 .5H31.5V32'/%3e%3c/svg%3e")`;return o.theme.name=="dark"?`${t}, ${o.theme.backgroundGradient}`:`${t}`}};
 
   mask-image: radial-gradient(at top, rgba(255, 255, 255, 0.8), transparent);
@@ -52,7 +52,7 @@ import{r as d,W as y,s as n,j as x,g as E,I as C,H as $,Z as S,B as A,a as z,b a
   padding-bottom: 8em;
   position: relative;
   z-index: 10;
-`,e=x.exports.jsx,r=x.exports.jsxs,b=x.exports.Fragment,Y=({children:o})=>r(q,{children:[e(H,{}),e(W,{children:o})]}),c=E({small:"640px",medium:"768px",large:"1024px"}),_=n.section`
+`,e=x.exports.jsx,r=x.exports.jsxs,b=x.exports.Fragment,Y=({children:o})=>r(G,{children:[e(q,{}),e(W,{children:o})]}),c=E({small:"640px",medium:"768px",large:"1024px"}),_=n.section`
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -207,7 +207,7 @@ import{r as d,W as y,s as n,j as x,g as E,I as C,H as $,Z as S,B as A,a as z,b a
   text-decoration: none;
   padding: 0.5em;
   font-weight: bold;
-`,ee=()=>{const[o,t]=d.exports.useState(!1),s=d.exports.useRef(null);async function i(){if(o){s.current&&s.current.classList.add("close"),await setTimeout(()=>t(!1),300);return}t(!0)}return r(b,{children:[o&&e(Z,{children:r(Q,{ref:s,children:[e(v,{onClick:i,"aria-label":"Fechar Navega\xE7\xE3o",children:e(C,{color:"#fff",size:45})}),e(h,{href:"#about-me",onClick:i,children:"Sobre mim"}),e(h,{href:"#experience",onClick:i,children:"Experi\xEAncia"}),e(h,{href:"#education",onClick:i,children:"Forma\xE7\xE3o Acad\xEAmica"}),e(h,{href:"#projects",onClick:i,children:"Projetos"})]})}),e(v,{onClick:i,"aria-label":"Abrir Navega\xE7\xE3o",children:e($,{color:"#fff",size:45})})]})},oe=({themeSwitcher:o})=>{const t=S();function s(){const i=t.name==="light"?"dark":"light";o(i)}return r(_,{children:[r(K,{children:[e("button",{"aria-label":"Mudar cor do site",onClick:s,children:t.name=="dark"?e("svg",{xmlns:"http://www.w3.org/2000/svg",fill:"none",viewBox:"0 0 24 24",stroke:"currentColor",strokeWidth:2,children:e("path",{strokeLinecap:"round",strokeLinejoin:"round",d:"M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"})}):e("svg",{xmlns:"http://www.w3.org/2000/svg",fill:"none",viewBox:"0 0 24 24",stroke:"currentColor",strokeWidth:2,children:e("path",{strokeLinecap:"round",strokeLinejoin:"round",d:"M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"})})}),e(ee,{})]}),r(V,{children:[e("img",{src:"/assets/profile_pic.webp",alt:"Sou eu!"}),e("span",{children:"Talvez esse site tenha um segredo, nunca se sabe..."})]}),e("h2",{children:"Oi! Meu nome \xE9 Guilherme da Silva Sousa."}),e("p",{children:"Desenvolvedor Fullstack no Mais Esports, atualmente cursando faculdade de Engenharia de Software na Universidade Est\xE1cio de S\xE1."})]})},re=n.nav`
+`,ee=()=>{const[o,t]=d.exports.useState(!1),s=d.exports.useRef(null);async function i(){if(o){s.current&&s.current.classList.add("close"),await setTimeout(()=>t(!1),300);return}t(!0)}return r(b,{children:[o&&e(Z,{children:r(Q,{ref:s,children:[e(v,{onClick:i,"aria-label":"Fechar Navega\xE7\xE3o",children:e(C,{color:"#fff",size:45})}),e(h,{href:"#about-me",onClick:i,children:"Sobre mim"}),e(h,{href:"#experience",onClick:i,children:"Experi\xEAncia"}),e(h,{href:"#education",onClick:i,children:"Forma\xE7\xE3o Acad\xEAmica"}),e(h,{href:"#projects",onClick:i,children:"Projetos"})]})}),e(v,{onClick:i,"aria-label":"Abrir Navega\xE7\xE3o",children:e($,{color:"#fff",size:45})})]})},oe=({themeSwitcher:o})=>{const t=S();function s(){const i=t.name==="light"?"dark":"light";o(i)}return r(_,{children:[r(K,{children:[e("button",{"aria-label":"Mudar cor do site",onClick:s,children:t.name=="dark"?e("svg",{xmlns:"http://www.w3.org/2000/svg",fill:"none",viewBox:"0 0 24 24",stroke:"currentColor",strokeWidth:2,children:e("path",{strokeLinecap:"round",strokeLinejoin:"round",d:"M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"})}):e("svg",{xmlns:"http://www.w3.org/2000/svg",fill:"none",viewBox:"0 0 24 24",stroke:"currentColor",strokeWidth:2,children:e("path",{strokeLinecap:"round",strokeLinejoin:"round",d:"M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"})})}),e(ee,{})]}),r(V,{children:[e("img",{src:"/assets/profile_pic.jpg",alt:"Sou eu!"}),e("span",{children:"Talvez esse site tenha um segredo, nunca se sabe..."})]}),e("h2",{children:"Oi! Meu nome \xE9 Guilherme da Silva Sousa."}),e("p",{children:"Desenvolvedor Fullstack no Mais Esports, atualmente cursando faculdade de Engenharia de Software na Universidade Est\xE1cio de S\xE1."})]})},re=n.nav`
   display: flex;
   align-items: center;
   justify-content: space-around;
@@ -410,37 +410,42 @@ Uso de tecnologias como Express, Prisma, Next.JS e algumas APIs de terceiros.`}]
   }
 `;var pe=[{institution:"Universidade Est\xE1cio de S\xE1 Rio de Janeiro (EAD)",period:"Fevereiro de 2022 - Atualmente",status:"Cursando",area:"Bacharelado em Engenharia de Software"},{institution:"Col\xE9gio Flama",period:"Fevereiro de 2019 - Dezembro de 2021",status:"Conclu\xEDdo",area:"Ensino M\xE9dio, T\xE9cnico em Inform\xE1tica"}];const he=()=>r(f,{id:"education",children:[e("h1",{children:"Forma\xE7\xE3o Acad\xEAmica"}),pe.map((o,t)=>r(ue,{children:[e("strong",{children:o.area}),e("span",{children:o.institution}),r("span",{children:["Estado: ",o.status]}),r("span",{children:["Per\xEDodo: ",o.period]})]},`academic-${t}`))]}),ge=n.div`
   width: 100%;
-  display: grid;
+  margin-top: 1em;
+
+  /* display: grid;
   grid-template-columns: repeat(2, 1fr);
   grid-gap: 1.5em;
-  margin-top: 1em;
 
   color: ${o=>o.theme.textColor} !important;
 
   ${c.lessThan("medium")`
     grid-template-columns: 1fr;
-  `};
+  `}; */
 `,fe=n.a`
   font-weight: 500 !important;
   transition: filter 0.2s ease-out;
   color: ${o=>o.theme.textColor} !important;
+  display: block;
+  margin: 0 0.5em;
 
   &:hover {
     filter: brightness(0.9);
   }
-`,xe=n.img`
-  width: 100%;
-  height: auto;
+
+  & + & {
+    margin-top: 2em;
+  }
+`;n.img`
+  width: 360px;
   aspect-ratio: 16/9;
   border-radius: 0.5em;
   border: 1px solid rgba(255, 255, 255, 0.3);
   object-fit: cover;
-`,be=n.div`
+`;const xe=n.div``,be=n.div`
   display: flex;
   align-items: center;
   font-weight: 700;
   font-size: 1.2em;
-  margin-top: 0.25em;
 
   svg {
     margin-left: 0.25em;
@@ -448,10 +453,12 @@ Uso de tecnologias como Express, Prisma, Next.JS e algumas APIs de terceiros.`}]
 `,ve=n.div`
   color: ${o=>o.theme.textColor} !important;
   font-size: 0.9em;
-`;var we=[{name:"Dramaland",imageUrl:"/assets/dramaland.webp",description:`Um portal de not\xEDcias, resenhas e opini\xE3o do mundo da dramaturgia coreana. 
+`;var we=[{name:"Hookpedia",description:`Um reposit\xF3rio de Hooks do React, feito pela comunidade para a comunidade. 
 
- Construido com Next.JS, TypeScript, Tailwind, OneSignal e Blogger API.`,url:"https://github.com/guilherssousa/dramaland",year:"2022"},{name:"Correio An\xF4nimo",imageUrl:"/assets/correio.webp",description:`Envie mensagens de forma an\xF4nima para suas pessoas queridas usando seus nomes de usu\xE1rio do Twitter. 
+ Construido com Next.JS e Chakra UI.`,url:"https://hookpedia.now.sh/",year:"2022"},{name:"Dramaland",description:`Um portal de not\xEDcias, resenhas e opini\xE3o do mundo da dramaturgia coreana. 
 
- Constru\xEDdo usando React, Firebase e Styled Components.`,url:"https://correioanonimo.com.br",year:"2021"},{name:"P44blo.",imageUrl:"/assets/p44blo.webp",description:`Ruan 'p44blo' \xE9 um editor de v\xEDdeos de Blumenau/SC. 
+ Construido com Next.JS, TypeScript, Tailwind, OneSignal e Blogger API.`,url:"https://github.com/guilherssousa/dramaland",year:"2022"},{name:"Correio An\xF4nimo",description:`Envie mensagens de forma an\xF4nima para suas pessoas queridas usando seus nomes de usu\xE1rio do Twitter. 
 
- Constru\xEDdo com HTML, CSS e Javascript Vanilla.`,url:"http://www.p44blo.com",year:"2020"}];const ke=()=>r(f,{id:"projects",children:[e("h1",{children:"Projetos"}),e(ge,{children:we.map((o,t)=>r(fe,{href:o.url,target:"_blank",rel:"noreferrer",children:[e(xe,{src:o.imageUrl,alt:o.name,loading:"lazy"}),r(be,{children:[o.name," ",e(L,{})]}),e(ve,{children:o.year}),e("p",{children:o.description})]},`project-${t}`))})]}),ye=({themeSwitcher:o})=>r(Y,{children:[e(oe,{themeSwitcher:o}),e(ne,{}),e(ie,{}),e(me,{}),e(he,{}),e(ke,{})]}),w=["ArrowUp","ArrowUp","ArrowDown","ArrowDown","ArrowLeft","ArrowRight","ArrowLeft","ArrowRight","b","a","Enter"];var u=0;function k(o){const{key:t}=o;if(u==w.length-1){window.alert("Voc\xEA achou o easter egg!"),u=0;return}t==w[u]?u++:u=0}function Ee(){const[o,t]=D("theme","dark");return d.exports.useEffect(()=>(window.addEventListener("keydown",k),()=>window.removeEventListener("keydown",k)),[]),e(b,{children:r(R,{theme:o=="light"?J:X,children:[e(G,{}),e(I,{basename:"/",children:e(P,{children:e(M,{path:"/",element:e(ye,{themeSwitcher:t})})})})]})})}B.render(e(O.StrictMode,{children:e(Ee,{})}),document.getElementById("root"));
+ Constru\xEDdo usando React, Firebase e Styled Components.`,url:"https://correioanonimo.com.br",year:"2021"},{name:"P44blo.",description:`Ruan 'p44blo' \xE9 um editor de v\xEDdeos de Blumenau/SC. 
+
+ Constru\xEDdo com HTML, CSS e Javascript Vanilla.`,url:"http://www.p44blo.com",year:"2020"}];const ke=()=>r(f,{id:"projects",children:[e("h1",{children:"Projetos"}),e(ge,{children:we.map((o,t)=>e(fe,{href:o.url,target:"_blank",rel:"noreferrer",children:r(xe,{children:[r(be,{children:[o.name," ",e(L,{})]}),e(ve,{children:o.year}),e("p",{children:o.description})]})},`project-${t}`))})]}),ye=({themeSwitcher:o})=>r(Y,{children:[e(oe,{themeSwitcher:o}),e(ne,{}),e(ie,{}),e(me,{}),e(he,{}),e(ke,{})]}),w=["ArrowUp","ArrowUp","ArrowDown","ArrowDown","ArrowLeft","ArrowRight","ArrowLeft","ArrowRight","b","a","Enter"];var u=0;function k(o){const{key:t}=o;if(u==w.length-1){window.alert("Voc\xEA achou o easter egg!"),u=0;return}t==w[u]?u++:u=0}function Ee(){const[o,t]=U("theme","dark");return d.exports.useEffect(()=>(window.addEventListener("keydown",k),()=>window.removeEventListener("keydown",k)),[]),e(b,{children:r(R,{theme:o=="light"?X:J,children:[e(H,{}),e(I,{basename:"/",children:e(P,{children:e(M,{path:"/",element:e(ye,{themeSwitcher:t})})})})]})})}B.render(e(N.StrictMode,{children:e(Ee,{})}),document.getElementById("root"));
