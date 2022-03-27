ca462cbfc127f4bcf9c0ca5a194f656c6126ba5f
guilherssousa <guilherssousa@gmail.com>
2022-03-26 22:08:02 -0300

    use vite plugin for pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,48 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: ["/assets/favicon.ico", "/assets/profile_pic.jpg"],
+      manifest: {
+        name: "Guilherme Sousa",
+        short_name: "Gui Sousa",
+        start_url: "/?home=true",
+        icons: [
+          {
+            src: "/icons/manifest-icon-192.maskable.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/icons/manifest-icon-192.maskable.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/icons/manifest-icon-512.maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/icons/manifest-icon-512.maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+        theme_color: "#f97316",
+        background_color: "#FEFEFE",
+        display: "fullscreen",
+        orientation: "portrait",
+      },
+    }),
+  ],
 });
